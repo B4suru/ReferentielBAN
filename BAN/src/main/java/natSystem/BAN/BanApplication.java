357bbfb691f6b69ba1c6d246a3ea538da7ff40a2
Maxime TARAUD <mtara@natsystem.fr>
2026-06-22 14:34:08 +0200

    Hello world work

--- BAN/src/main/java/natSystem/BAN/BanApplication.java
+++ BAN/src/main/java/natSystem/BAN/BanApplication.java
@@ -1,7 +1,13 @@
 package natSystem.BAN;
 
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.job.parameters.JobParameters;
+import org.springframework.batch.core.job.parameters.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BanApplication {
@@ -9,5 +15,14 @@ public class BanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BanApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner run(JobLauncher launcher, Job helloWorldJob) {
+		return args -> {
+			JobParameters params = new JobParametersBuilder()
+					.addLong("startAt", System.currentTimeMillis())
+					.toJobParameters();
+			launcher.run(helloWorldJob, params);
+		};
+	}
 }
